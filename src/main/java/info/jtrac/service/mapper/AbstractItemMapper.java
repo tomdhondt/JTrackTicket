dbff3826556ed9c5cbe1ea3de863b3f0e59266f5
tomdhondt <tom.dhondt@hotmail.com>
2014-05-11 16:21:18 +0200

    mapping AbstractItemDTO - AbstractItem

--- src/main/java/info/jtrac/service/mapper/AbstractItemMapper.java
+++ src/main/java/info/jtrac/service/mapper/AbstractItemMapper.java
@@ -11,8 +11,6 @@ import main.java.info.jtrac.domain.User;
 import main.java.info.jtrac.exception.data.DataDAOException;
 import main.java.info.jtrac.exception.mapper.MapperException;
 import main.java.info.jtrac.service.dto.AbstractItemDTO;
-import main.java.info.jtrac.service.dto.UserDTO;
-import main.java.info.jtrac.service.manager.IManager;
 import main.java.info.jtrac.util.MappingUtil;
 
 @SuppressWarnings("unchecked")
@@ -27,18 +25,19 @@ public class AbstractItemMapper extends AbstractMapper<AbstractItem, AbstractIte
 		/*
 		 * Instance members
 		 */
-
 		AbstractItem object = null;
 		if(null != dto){
 			object = new AbstractItem() {
+				/**
+				 * Serial version ID
+				 */
+				private static final long serialVersionUID = 3582922532882713738L;
 				@Override
 				public Space getSpace() {
-					// TODO Auto-generated method stub
 					return null;
 				}
 				@Override
 				public String getRefId() {
-					// TODO Auto-generated method stub
 					return null;
 				}
 			};
@@ -49,33 +48,31 @@ public class AbstractItemMapper extends AbstractMapper<AbstractItem, AbstractIte
 			object.setLoggedBy(this.getUser(MappingUtil.mapStringToLong(dto.getUser_loggedBy_Id())));
 			object.setAssignedTo(this.getUser(MappingUtil.mapStringToLong(dto.getUser_assignedTo_Id())));
 			object.setPlannedEffort(MappingUtil.mapStringToDouble(dto.getPlannedEffort()));
-			object.setStatus(MappingUtil.mapStringToLong(dto.getStatus()));
-//			object.setSeverity();				// Integer
-//			object.setPriority();				// Integer
-//			object.setCusInt01();				// Integer
-//			object.setCusInt02();				// Integer
-//			object.setCusInt03();				// Integer
-//			object.setCusInt04();				// Integer
-//			object.setCusInt05();				// Integer
-//			object.setCusInt06();				// Integer
-//			object.setCusInt07();				// Integer
-//			object.setCusInt08();				// Integer
-//			object.setCusInt09();				// Integer
-//			object.setCusInt10();				// Integer
-//			object.setCusDbl01();				// Double
-//			object.setCusDbl02();				// Double
-//			object.setCusDbl03();				// Double
-//			object.setCusStr01();				// String
-//			object.setCusStr02();				// String
-//			object.setCusStr03();				// String
-//			object.setCusStr04();				// String
-//			object.setCusStr05();				// String
+			object.setStatus(MappingUtil.mapStringToInteger(dto.getStatus()));
+			object.setSeverity(MappingUtil.mapStringToInteger(dto.getStatus()));				// Integer
+			object.setPriority(MappingUtil.mapStringToInteger(dto.getPriority()));				// Integer
+			object.setCusInt01(MappingUtil.mapStringToInteger(dto.getCusInt01()));				// Integer
+			object.setCusInt02(MappingUtil.mapStringToInteger(dto.getCusInt02()));				// Integer
+			object.setCusInt03(MappingUtil.mapStringToInteger(dto.getCusInt03()));				// Integer
+			object.setCusInt04(MappingUtil.mapStringToInteger(dto.getCusInt04()));				// Integer
+			object.setCusInt05(MappingUtil.mapStringToInteger(dto.getCusInt05()));				// Integer
+			object.setCusInt06(MappingUtil.mapStringToInteger(dto.getCusInt06()));				// Integer
+			object.setCusInt07(MappingUtil.mapStringToInteger(dto.getCusInt07()));				// Integer
+			object.setCusInt08(MappingUtil.mapStringToInteger(dto.getCusInt08()));				// Integer
+			object.setCusInt09(MappingUtil.mapStringToInteger(dto.getCusInt09()));				// Integer
+			object.setCusInt10(MappingUtil.mapStringToInteger(dto.getCusInt10()));				// Integer
+			object.setCusDbl01(MappingUtil.mapStringToDouble(dto.getCusDbl01()));				// Double
+			object.setCusDbl02(MappingUtil.mapStringToDouble(dto.getCusDbl01()));				// Double
+			object.setCusDbl03(MappingUtil.mapStringToDouble(dto.getCusDbl01()));				// Double
+			object.setCusStr01(dto.getCusStr01());												// String
+			object.setCusStr02(dto.getCusStr01());												// String
+			object.setCusStr03(dto.getCusStr01());												// String
+			object.setCusStr04(dto.getCusStr01());												// String
+			object.setCusStr05(dto.getCusStr01());												// String
 //			object.setCusTim01();				// Date
 //			object.setCusTim02();				// Date
 //			object.setCusTim03();    			// Date
-		
 		}
-		
 		return object;
 	}
 	/**
@@ -85,30 +82,51 @@ public class AbstractItemMapper extends AbstractMapper<AbstractItem, AbstractIte
 	 */
 	@Override
 	public AbstractItemDTO mapToDTO(AbstractItem object) throws MapperException {
-	//		object.setSeverity();				// Integer
-	//		object.setPriority();				// Integer
-	//		object.setCusInt01();				// Integer
-	//		object.setCusInt02();				// Integer
-	//		object.setCusInt03();				// Integer
-	//		object.setCusInt04();				// Integer
-	//		object.setCusInt05();				// Integer
-	//		object.setCusInt06();				// Integer
-	//		object.setCusInt07();				// Integer
-	//		object.setCusInt08();				// Integer
-	//		object.setCusInt09();				// Integer
-	//		object.setCusInt10();				// Integer
-	//		object.setCusDbl01();				// Double
-	//		object.setCusDbl02();				// Double
-	//		object.setCusDbl03();				// Double
-	//		object.setCusStr01();				// String
-	//		object.setCusStr02();				// String
-	//		object.setCusStr03();				// String
-	//		object.setCusStr04();				// String
-	//		object.setCusStr05();				// String
-	//		object.setCusTim01();				// Date
-	//		object.setCusTim02();				// Date
-	//		object.setCusTim03();    			// Date
-		return null;
+		/*
+		 * Instance members
+		 */
+		AbstractItemDTO dto = null;
+		if(null != object){
+			dto.setId(MappingUtil.mapLongToString(object.getId()));
+			dto.setParent_Item_Id(MappingUtil.mapLongToString(object.getParent().getId()));
+			dto.setParent_Item_EditReason(object.getParent().getEditReason());
+			dto.setSummary(dto.getSummary());
+			dto.setDetail(dto.getDetail());
+			dto.setUser_loggedBy_Id(MappingUtil.mapLongToString(object.getLoggedBy().getId()));
+			dto.setUser_loggedBy_name(object.getLoggedBy().getName());
+			dto.setUser_loggedBy_loginName(object.getLoggedBy().getLoginName());
+			dto.setUser_loggedBy_email(object.getLoggedBy().getEmail());
+			dto.setUser_assignedTo_Id(MappingUtil.mapLongToString(object.getAssignedTo().getId()));
+			dto.setUser_assignedTo_name(object.getAssignedTo().getName());
+			dto.setUser_assignedTo_loginName(object.getAssignedTo().getLoginName());
+			dto.setUser_assignedTo_email(object.getAssignedTo().getEmail());
+			dto.setPlannedEffort(MappingUtil.mapDoubleToString(object.getPlannedEffort()));
+			dto.setStatus(MappingUtil.mapIntegerToString(object.getStatus()));
+			dto.setSeverity(MappingUtil.mapIntegerToString(object.getSeverity()));
+			dto.setPriority(MappingUtil.mapIntegerToString(object.getPriority()));				// Integer
+			dto.setCusInt01(MappingUtil.mapIntegerToString(object.getCusInt01()));				// Integer
+			dto.setCusInt02(MappingUtil.mapIntegerToString(object.getCusInt02()));				// Integer
+			dto.setCusInt03(MappingUtil.mapIntegerToString(object.getCusInt03()));				// Integer
+			dto.setCusInt04(MappingUtil.mapIntegerToString(object.getCusInt04()));				// Integer
+			dto.setCusInt05(MappingUtil.mapIntegerToString(object.getCusInt05()));				// Integer
+			dto.setCusInt06(MappingUtil.mapIntegerToString(object.getCusInt06()));				// Integer
+			dto.setCusInt07(MappingUtil.mapIntegerToString(object.getCusInt07()));				// Integer
+			dto.setCusInt08(MappingUtil.mapIntegerToString(object.getCusInt08()));				// Integer
+			dto.setCusInt09(MappingUtil.mapIntegerToString(object.getCusInt09()));				// Integer
+			dto.setCusInt10(MappingUtil.mapIntegerToString(object.getCusInt10()));				// Integer
+			dto.setCusDbl01(MappingUtil.mapDoubleToString(object.getCusDbl01()));				// Double
+			dto.setCusDbl02(MappingUtil.mapDoubleToString(object.getCusDbl02()));				// Double
+			dto.setCusDbl03(MappingUtil.mapDoubleToString(object.getCusDbl03()));				// Double
+			dto.setCusStr01(object.getCusStr01());												// String
+			dto.setCusStr02(object.getCusStr02());												// String
+			dto.setCusStr03(object.getCusStr03());												// String
+			dto.setCusStr04(object.getCusStr04());												// String
+			dto.setCusStr05(object.getCusStr05());												// String
+	//		dto.setCusTim01();				// Date
+	//		dto.setCusTim02();				// Date
+	//		dto.setCusTim03();    			// Date
+		}
+		return dto;
 	}
 	/*
 	 * Method will get the reference user out the database
