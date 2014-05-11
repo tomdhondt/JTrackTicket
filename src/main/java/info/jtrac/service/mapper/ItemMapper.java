c86848e1dcb78b645c54cf52d9d35f3d5d458848
tomdhondt <tom.dhondt@hotmail.com>
2014-05-11 18:50:28 +0200

    Added ItemMapper

---
+++ src/main/java/info/jtrac/service/mapper/ItemMapper.java
@@ -0,0 +1,21 @@
+package main.java.info.jtrac.service.mapper;
+
+import main.java.info.jtrac.domain.Item;
+import main.java.info.jtrac.exception.mapper.MapperException;
+import main.java.info.jtrac.service.dto.ItemDTO;
+
+public class ItemMapper extends AbstractMapper<Item, ItemDTO>{
+
+	@Override
+	public Item mapToObject(ItemDTO dto) throws MapperException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ItemDTO mapToDTO(Item object) throws MapperException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
